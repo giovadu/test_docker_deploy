97616a305bc3de09677df514840bf83b46e33d69
Felipe Osorio <iamfelipeo@gmail.com>
2024-09-05 15:04:42 -0500

    server configuration

--- main.go
+++ main.go
@@ -20,7 +20,9 @@ func main() {
 }
 
 func processEvents(id int) {
+
 	startTime := time.Now() // Tiempo de inicio del ciclo
+
 	eventRepom, err := repositories.GetEventsWithLimit(id, 700)
 	if err != nil {
 		log.Printf("[Worker %d] error obteniendo eventos: %v", id, err)
@@ -28,8 +30,11 @@ func processEvents(id int) {
 	}
 
 	var wgEvents sync.WaitGroup
+
+	errorChannel := make(chan error, len(eventRepom)) // Canal para capturar errores
 	var successCount, failCount int
 
+	// Slice para acumular los mensajes de verificación
 	var verificationMessages []string
 	var unregisteredTokens []string
 
@@ -40,32 +45,35 @@ func processEvents(id int) {
 
 			users, err := repositories.GetUsersToSendNotifications(event.Plate)
 			if err != nil {
-				log.Printf("[Worker %d] error obteniendo usuarios: %v", id, err)
+				errorChannel <- fmt.Errorf("error obteniendo usuarios para %s: %v", event.Plate, err)
+				failCount++
 				return
 			}
 
 			var wgUsers sync.WaitGroup
 			for j := 0; j < len(users); j++ {
 				wgUsers.Add(1)
+				go func(user models.UserInfo) {
+					defer wgUsers.Done()
 
-				defer wgUsers.Done()
+					alert := fmt.Sprintf("Alerta Vehículo %s", event.Plate)
 
-				alert := fmt.Sprintf("Alerta Vehículo %s", event.Plate)
+					err := services.SendFirebaseNotification(user.Token, alert, event.Event)
+					var eventMessage string
+					if err != nil {
+						unregisteredTokens = append(unregisteredTokens, user.Token)
+						eventMessage = fmt.Sprintf("%s %s %d Enviando push a %s %s Dispositivo: %s error: %v", event.ServerTime, user.AppName, event.ID, user.Name, user.So, event.Plate, err)
+						errorChannel <- err
+						failCount++
+					} else {
+						eventMessage = fmt.Sprintf("%s %s %d Enviando push a %s %s Dispositivo: %s exitoso", event.ServerTime, user.AppName, event.ID, user.Name, user.So, event.Plate)
+						successCount++
+					}
 
-				err := services.SendFirebaseNotification(users[j].Token, alert, event.Event)
-				var eventMessage string
-				if err != nil {
-					unregisteredTokens = append(unregisteredTokens, users[j].Token)
-					eventMessage = fmt.Sprintf("%s %s %d Enviando push a %s %s Dispositivo: %s error: %v", event.ServerTime, users[j].AppName, event.ID, users[j].Name, users[j].So, event.Plate, err)
-					failCount++
-				} else {
-					eventMessage = fmt.Sprintf("%s %s %d Enviando push a %s %s Dispositivo: %s exitoso", event.ServerTime, users[j].AppName, event.ID, users[j].Name, users[j].So, event.Plate)
-					successCount++
-				}
+					// Acumular los mensajes de verificación
+					verificationMessages = append(verificationMessages, eventMessage)
 
-				// Acumular los mensajes de verificación
-				verificationMessages = append(verificationMessages, eventMessage)
-
+				}(users[j])
 			}
 
 			// Espera a que todas las goroutines de usuarios terminen
@@ -75,10 +83,14 @@ func processEvents(id int) {
 
 	// Espera a que todas las goroutines de eventos terminen
 	wgEvents.Wait()
+	close(errorChannel)
+
+	// Maneja los errores capturados
+	log.Println(len(errorChannel))
 
 	// Insertar todos los mensajes de verificación en la base de datos
 	if len(verificationMessages) > 0 {
-		repositories.BatchInsertVerificationMessages("gpsec", verificationMessages)
+		go repositories.BatchInsertVerificationMessages("gpsec", verificationMessages)
 	}
 	if len(unregisteredTokens) > 0 {
 		err := repositories.BatchDeleteTokens(unregisteredTokens)
@@ -95,5 +107,3 @@ func processEvents(id int) {
 	log.Printf("[Worker %d] Ciclo completado: %d eventos procesados en %v", id, totalEvents, totalTime)
 	log.Printf("[Worker %d] Ciclo completado: %d eventos procesados, %d exitosos, %d fallidos", id, totalEvents, successCount, failCount)
 }
-
-//1010 eventos procesados en 38.79007075s
